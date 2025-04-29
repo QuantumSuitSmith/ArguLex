2f08a2d264fba8ff572199e98fdd453234fc145b
akshaydataenthu <chintalaakshay030@gmail.com>
2025-04-29 23:11:57 +0530

    Add files via upload

---
+++ FrontEnd/createaccount.js
@@ -0,0 +1,6 @@
+document.getElementById("signup-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the form from submitting the usual way
+    // You can handle form data submission here
+    alert("Account created successfully!");
+  });
+  
