75b5ee9a8583a81fda72e53393e379da8c46935c
akshaydataenthu <chintalaakshay030@gmail.com>
2025-04-29 22:54:59 +0530

    Delete createaccount.js

--- createaccount.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("signup-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the form from submitting the usual way
-    // You can handle form data submission here
-    alert("Account created successfully!");
-  });
-  
