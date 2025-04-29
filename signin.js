06051c97c4ddc2d3ae4cac0f0826f4383dbd6165
akshaydataenthu <chintalaakshay030@gmail.com>
2025-04-29 23:13:34 +0530

    Delete signin.js

--- signin.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("signin-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the form from submitting the usual way
-    // Handle form submission (authentication logic can be added later)
-    alert("Signed in successfully!");
-  });
-  
