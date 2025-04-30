c153169423c71a7b20cbd97e1429a78cb6d6f6a3
akshaydataenthu <chintalaakshay030@gmail.com>
2025-04-30 14:00:18 +0530

    Update createaccount.js

--- FrontEnd/createaccount.js
+++ FrontEnd/createaccount.js
@@ -1,6 +1,5 @@
 document.getElementById("signup-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the form from submitting the usual way
-    // You can handle form data submission here
+    event.preventDefault(); 
     alert("Account created successfully!");
   });
   
